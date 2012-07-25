be7d38d30ad23bd36f6f83dee29ea687cf566ae2
cheng04119@gmail.com <cheng04119@gmail.com@d2da3b23-7633-ad81-7ceb-c21e92849739>
2012-07-25 03:18:17 +0000

    数据库定时备份

---
+++ trunk/src/data/util/TaskBackup.java
@@ -0,0 +1,34 @@
+package data.util;
+
+import java.util.Date;
+import java.util.Timer;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.lang.time.DateUtils;
+
+import data.service.ManageDataService;
+
+public class TaskBackup implements ServletContextListener{
+	public static final long PERIOD_DAY = DateUtils.MILLIS_IN_DAY;
+	
+	private Timer timer;
+
+	public void contextDestroyed(ServletContextEvent event) {
+		// TODO Auto-generated method stub
+		timer.cancel();
+	}
+
+	public void contextInitialized(ServletContextEvent event) {
+		// TODO Auto-generated method stub
+		timer = new Timer("数据库备份",true);
+		Date date = new Date();
+		date.setHours(0);
+		date.setMinutes(0);
+		date.setSeconds(0);
+		
+		timer.schedule(new BackupData(),date,PERIOD_DAY);
+	}
+	
+}
