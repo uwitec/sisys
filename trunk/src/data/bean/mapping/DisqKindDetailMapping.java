66a24e1cd4cc1578d5d84940c48e14f83f8f7ab5
xcjh1518@gmail.com <xcjh1518@gmail.com@d2da3b23-7633-ad81-7ceb-c21e92849739>
2012-07-25 02:37:06 +0000

    数据库新表的bean，mapping，dao

---
+++ trunk/src/data/bean/mapping/DisqKindDetailMapping.java
@@ -0,0 +1,26 @@
+package data.bean.mapping;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import data.bean.DisqKindDetail;
+
+public class DisqKindDetailMapping extends BasicMapping<DisqKindDetail> {
+	
+	@Override
+	public DisqKindDetail mapping(ResultSet rs) {
+		
+		DisqKindDetail disqKindDetail = new DisqKindDetail();
+		try {
+			disqKindDetail.setDisqKId(rs.getInt("disqKId"));
+			disqKindDetail.setId(rs.getInt("Id"));
+			disqKindDetail.setTime(rs.getDate("time"));
+			disqKindDetail.setDisqKNum(rs.getInt("disqKNum"));
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		return disqKindDetail;
+		
+	}
+
+}
