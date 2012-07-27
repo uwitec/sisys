40f051a1d109120d0d1645c7a737cee0d4696bb4
cheng04119 <cheng04119@gmail.com>
2012-07-27 03:49:55 +0000

    完善定时备份

--- src/data/util/TaskBackup.java
+++ src/data/util/TaskBackup.java
@@ -1,5 +1,6 @@
 package data.util;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Timer;
 
@@ -22,12 +23,15 @@ public class TaskBackup implements ServletContextListener{
 	public void contextInitialized(ServletContextEvent event) {
 		// TODO Auto-generated method stub
 		timer = new Timer("数据库备份",true);
-		Date date = new Date();
-		date.setHours(0);
-		date.setMinutes(0);
-		date.setSeconds(0);
+		Calendar cal = Calendar.getInstance();
+		long now = cal.getTimeInMillis();
+		cal.add(Calendar.DATE, 1);
+		cal.set(Calendar.HOUR, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		long next = cal.getTimeInMillis();
 		
-		timer.schedule(new BackupData(),date,PERIOD_DAY);
+		timer.schedule(new BackupData(),next-now,PERIOD_DAY);
 	}
 	
 }
